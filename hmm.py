6b246697c79dfbaf70376d1b81f200bb8331b4c8
Bow-Yaw <bowyaw@bitbucket.org>
2019-01-22 16:26:27 +0800

    add Z3 python script for HMM examples

---
+++ hmm.py
@@ -0,0 +1,91 @@
+# to execute: do
+# $ PYTHONPATH=<path to python Z3> python hmm.py
+
+from sympy.interactive.printing import init_printing
+init_printing()
+from sympy.matrices import Matrix
+from sympy import Rational
+from z3 import *
+
+TR = Matrix ([[Rational (2, 3), Rational (1, 6), Rational (1, 6)],
+              [Rational (1, 3), Rational (1, 3), Rational (1, 3)],
+              [Rational (1, 6), Rational (1, 6), Rational (2, 3)]])
+
+# prior knowledge: family member, contagious disease
+
+p0 = Matrix ([[1, 0, 0]])
+p1 = Matrix ([[0, 0, 1]])
+
+r0 = p0 * TR
+r1 = p1 * TR
+
+print (r0)
+print (r1)
+
+# prior knowledge: disease contraction rate is p, independent
+
+rate = Real ('rate')
+p_ind = Matrix ([[(1 - rate) * (1 - rate),
+                  2 * rate * (1 - rate),
+                  rate * rate]])
+r_ = p_ind * TR
+r_ind = [[r_[0].simplify (), r_[1].simplify (), r_[2].simplify ()]]
+
+cond = 2 * rate * (1 - rate) + rate * rate
+p_con = Matrix ([[0,
+                  2 * rate * (1- rate) / cond,
+                  rate * rate / cond]])
+r__ = p_con * TR
+r_con = [[r__[0].simplify (), r__[1].simplify (), r__[2].simplify ()]]
+
+print r_ind
+print r_con
+
+solver = Solver ()
+solver.add (0 <= rate)
+solver.add (rate <= 1)
+solver.push ()
+
+print r_ind[0][0] <= 2 * r_con[0][0]
+print r_con[0][0] <= 2 * r_ind[0][0]
+solver.add (Not (And (
+    rate**2/6 - 2*rate/3 + 2/3 <= (-3*rate + 4)/(3*(-rate + 2)),
+    (-3*rate + 4)/(6*(-rate + 2)) <= rate**2/3 - 4*rate/3 + 4/3)))
+print solver.check ()
+print solver.model ()
+"""
+rate=3/4
+print rate**2/6 - 2*rate/3 + 2/3 <= (-3*rate + 4)/(3*(-rate + 2))
+print (-3*rate + 4)/(6*(-rate + 2)) <= rate**2/3 - 4*rate/3 + 4/3
+"""
+
+solver.pop ()
+solver.push ()
+print r_ind[0][1] <= 2 * r_con[0][1]
+print r_con[0][1] <= 2 * r_ind[0][1]
+solver.add (Not (And (
+    -rate**2/3 + rate/3 + 1/6 <= (-3*rate + 4)/(3*(-rate + 2)),
+    (-3*rate + 4)/(6*(-rate + 2)) <= -2*rate**2/3 + 2*rate/3 + 1/3)))
+print solver.check ()
+print solver.model ()
+"""
+rate=1/2
+print -rate**2/3 + rate/3 + 1/6, (-3*rate + 4)/(3*(-rate + 2))
+print (-3*rate + 4)/(6*(-rate + 2)), -2*rate**2/3 + 2*rate/3 + 1/3
+"""
+
+solver.pop ()
+solver.push ()
+print r_ind[0][2] <= 2 * r_con[0][2]
+print r_con[0][2] <= 2 * r_ind[0][2]
+solver.add (Not (And (
+    rate**2/6 + rate/3 + 1/6 <= 4/(3*(-rate + 2)),
+    2/(3*(-rate + 2)) <= rate**2/3 + 2*rate/3 + 1/3)))
+print solver.check ()
+print solver.model ()
+"""
+rate=1/2
+print rate**2/6 + rate/3 + 1/6, 4/(3*(-rate + 2))
+print 2/(3*(-rate + 2)), rate**2/3 + 2*rate/3 + 1/3
+"""
+
